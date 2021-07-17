bc8c6dd5201a41bd500948b0882dfa3cf8318cb2
namhoonchoe <sysynh2012@gmail.com>
2021-07-17 11:54:53 +0900

    search component #5
    redirection

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch,Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Detail from "@screens/Detail";
 import Header from "./Header";
 import Home from "@screens/Home";
@@ -7,15 +7,19 @@ import Movie from "@screens/Movie";
 import Person from "@screens/Person"
 import TV from "@screens/TV";
 import Search from "@screens/Search";
+import { useSelector } from "react-redux";
+import { selectSearch } from "@redux/searchSlice"
 
 const RoootRouter: React.FC = () => {
+  const searchTerm = useSelector(selectSearch)
+
   return (
     <Router>
       <>
       <Header/>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/search" component={Search} />
+          <Route path="/search" component={Search}/>
           <Route path="/tag" component={Search} />
           <Route path="/movie/:id" component={Detail} />
           <Route path="/tv/:id" component={Detail} />
@@ -23,8 +27,10 @@ const RoootRouter: React.FC = () => {
           <Route path="/movie" component={Movie} />
           <Route path="/tv" component={TV} />
           <Route path="/person" component={Person} />
-          <Redirect from="*" to="/" />
         </Switch>
+        <Route path="/*">
+          {searchTerm !=="" && <Redirect to="/search"/>}
+        </Route>
       </>
     </Router>
   );
---
+++ src/Components/SearchRoute.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Redirect } from "react-router-dom";
+/*
+
+interface IRouteProps {
+  redirection:boolean
+  location:string
+}
+
+const searchTerm = useSelector(selectSearch)
+
+const SearchRoute:React.FC<IRouteProps> = (props) => {
+  const { redirection, location } = props
+  if(redirection) {
+    return <Redirect  from="*" 
+                      to={{
+                      pathname:"/search",
+                      state:{location}
+                      }}/>
+  }
+  return (<Route {...props}/>)
+}
+*/
+
+export default  {}
